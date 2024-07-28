c8de896d116c6fe02a8ce5fc0e22ff2c08624c92
MaindoVN <134129396+MaindoVN@users.noreply.github.com>
2024-07-28 12:19:15 +0700

    Update setup_ssh.sh

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -9,6 +9,9 @@ sudo apt install -y openssh-server
 # Thiết lập mật khẩu cho root
 echo "root:123456" | sudo chpasswd
 
+# Hỏi người dùng muốn đặt port nào
+read -p "Enter the port number you want to use for SSH: " ssh_port
+
 # Chỉnh sửa file sshd_config để cho phép đăng nhập root và thay đổi port
 sudo sed -i '/^#PermitRootLogin /d' /etc/ssh/sshd_config
 sudo sed -i '/^PermitRootLogin /d' /etc/ssh/sshd_config
@@ -16,9 +19,9 @@ echo 'PermitRootLogin yes' | sudo tee -a /etc/ssh/sshd_config
 
 sudo sed -i '/^#Port /d' /etc/ssh/sshd_config
 sudo sed -i '/^Port /d' /etc/ssh/sshd_config
-echo 'Port 40905' | sudo tee -a /etc/ssh/sshd_config
+echo "Port $ssh_port" | sudo tee -a /etc/ssh/sshd_config
 
 # Khởi động lại dịch vụ SSH
 sudo systemctl restart ssh
 
-echo "SSH server has been configured. Root login is enabled and port has been changed to 40905."
+echo "SSH server has been configured. Root login is enabled and port has been changed to $ssh_port."
